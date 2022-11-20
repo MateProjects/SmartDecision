2c04da3f5146e40f0d4a2d8c9528e684aa7a5aac
e.karametulova <e.karametulova@m.sharksw.com>
2022-11-20 21:12:20 +0200

    fix

--- src/conmponents/App/App.js
+++ src/conmponents/App/App.js
@@ -2,8 +2,8 @@
 // import{BrowserRouter,  Route, Switch } from 'react-router-dom';
 // import './App.css';
 // import Form from '../form/formInitialize';
-import { ThemeProvider } from '@mui/material';
-import  theme  from '../../config/theme';
+import { ThemeProvider } from "@mui/material";
+import theme from "../../config/theme";
 
 // export default function App() {
 //   return (
@@ -17,55 +17,59 @@ import  theme  from '../../config/theme';
 //   );
 // }
 
-import { Switch, Route } from 'react-router-dom';
-import { Suspense, useEffect } from 'react';
+import { Switch, Route } from "react-router-dom";
+import { Suspense, useEffect } from "react";
 // import { ToastContainer } from 'react-toastify';
-import routes from '../../routes';
+import routes from "../../routes";
 
-import { useDispatch } from 'react-redux';
-import  authOperations  from '../../redux/auth/authOperations';
+import { useDispatch } from "react-redux";
+import authOperations from "../../redux/auth/authOperations";
 
-import PrivateRoute from '../privateRoute/privateRoute';
-import PublicRoute from '../publicRoute/publicRoute';
-import Layout from '../../components/loyout/loyout';
-
+import PrivateRoute from "../privateRoute/privateRoute";
+import PublicRoute from "../publicRoute/publicRoute";
+import Layout from "../../components/loyout/loyout";
 
 function App() {
   const dispatch = useDispatch();
 
- 
-
   useEffect(() => {
-
-      // auth.onAuthStateChanged(userAuth => {
-      //     if(userAuth !== null) 
-      //     localStorage.setItem('token', userAuth.uid);
-      // });
-
-  },[dispatch]);
-  
+    // auth.onAuthStateChanged(userAuth => {
+    //     if(userAuth !== null)
+    //     localStorage.setItem('token', userAuth.uid);
+    // });
+  }, [dispatch]);
 
   return (
-    
     <>
-         <ThemeProvider theme={theme}>
-            <Layout>
-              <Suspense fallback={<div>"Loading.."</div>}>
-              <Switch>
-              {routes.map((route => route.privated?
-               <PrivateRoute key={route.path} component={route.component} {...route}></PrivateRoute>
-               :
-               <PublicRoute key={route.path}  restricted={true} component={route.component} {...route}></PublicRoute>))
-              }
-          </Switch>
-             </Suspense>
-           </Layout>
-           </ThemeProvider>
-        </>
-  )
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Suspense fallback={<div>"Loading.."</div>}>
+            <Switch>
+              {routes.map((route) =>
+                route.privated ? (
+                  <PrivateRoute
+                    key={route.path}
+                    component={route.component}
+                    {...route}
+                  ></PrivateRoute>
+                ) : (
+                  <PublicRoute
+                    key={route.path}
+                    restricted={true}
+                    component={route.component}
+                    {...route}
+                  ></PublicRoute>
+                )
+              )}
+            </Switch>
+          </Suspense>
+        </Layout>
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default App
+export default App;
 // import Loader from './Loader';
 
 // export default function App() {
@@ -79,7 +83,6 @@ export default App
 
 //   }, []);
 
-  
 // console.log(routes)
 
 //   return (
@@ -89,7 +92,7 @@ export default App
 //       <Suspense >
 //         <Switch>
 //           {routes.map(
-         
+
 //             ({ path, isProtected, redirectTo, exact, component: Component }) =>
 //               isProtected === null ? (
 //                 <Route
